8ed73c98888a317172572b03e640a2027a5f2706
Camille Franc <Camille.franc.pro@gmail.com>
2018-08-16 10:55:09 +0200

    airbnb first part

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+flats_attributes = [
+  {
+    name: 'Light & Spacious Garden Flat London',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 75,
+    number_of_guest: 3
+  },
+  {
+    name: 'Inner-city charming appartment',
+    address: '10 Downing Street',
+    description: 'Beautiful and spacious appartment that; one bedroom, and seperate kitchen',
+    price_per_night: 92,
+    number_of_guest: 2
+  },
+  {
+    name: 'Cosy flat on 5th avenue',
+    address: '5th avenue 10001 NewYork',
+    description: 'Little but refreshing appartment that makes you feel at home',
+    price_per_night: 92,
+    number_of_guest: 3
+  },
+  {
+    name: 'Spacious and full of light loft in Manhattan',
+    address: '185 Clinton St 10007 NewYork',
+    description: 'A bright and spacious appartment that will make you feel at home and at ease!',
+    price_per_night: 92,
+    number_of_guest: 4
+  }
+]
+
+Flat.create!(flats_attributes)
+puts 'Finished!'
